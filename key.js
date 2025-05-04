dcd75e2ec16dc7fa0263ba9cb773185d8830695e
North-Nur <apipth2233@gmail.com>
2025-05-04 15:34:34 +0700

    Update key.js

--- key.js
+++ key.js
@@ -1,28 +1,9 @@
 // key.js
 const keys = [
   {
-    script: 'NorthD_idcard',
-    key: "ss",
-    ip: "127.0.0.1"
-  },
-
-
-  {
     script: 'NorthsD_idcard',
     key: "validkey2",
     ip: "184.22.226.92"
-  },
-
-  {
-    script: 'NorthD_idcard',
-    key: "ffgyj",
-    ip: "1.02.20"
-  },
-  
-  {
-    script: 'NorthD_idcard',
-    key: "drgdrg",
-    ip: "111.111.1255"
   },
 
 
