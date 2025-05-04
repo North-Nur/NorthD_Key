83eb56d4796a96b5e2d860ea47ebc73d5b1c63b6
North-Nur <apipth2233@gmail.com>
2025-05-04 15:42:15 +0700

    Update key.js

--- key.js
+++ key.js
@@ -2,10 +2,16 @@
 const keys = [
   {
     script: 'NorthsD_idcard',
-    key: "validkey2",
+    key: "ND_zsqFpsQv9LC69ZzE",
     ip: "49.228.252.131"
   },
 
+  {
+    script: 'NorthsD_idcard',
+    key: "ND_4IYBAkqSZmKpJZn",
+    ip: "191.96.93.219 "
+  },
+  
 
 ];
 
